3a1464fe67fe090904d7b6548e18752159f653b9
OlegRabyshev <oryabyshev@gmail.com>
2024-01-20 12:53:12 +0300

    [TinkovQuotes] adding search functionality

--- app/src/main/java/com/example/tinkovquotes/presentation/screen/quotes/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/tinkovquotes/presentation/screen/quotes/viewmodel/QuotesViewModel.kt
@@ -9,18 +9,54 @@ class QuotesViewModel(
     private val quotesInteractor: QuotesInteractor
 ) : ViewModel() {
 
+    private lateinit var fullQuoteList: List<QuoteItem>
+
+    private val _searchTextStateFlow = MutableStateFlow("")
+    val searchTextStateFlow = _searchTextStateFlow
+
     private val _quotesListStateFlow = MutableStateFlow<List<QuoteItem>>(emptyList())
     val quotesListStateFlow = _quotesListStateFlow
 
-    fun updateQuotesList() {
-        val quotesList = quotesInteractor.getQuotesList()
-
-        _quotesListStateFlow.value = quotesList
+    init {
+        updateQuotesList()
     }
 
     fun onFavoriteChange(quoteId: Int, isFavorite: Boolean) {
         quotesInteractor.updateFavoriteState(quoteId, isFavorite)
 
         updateQuotesList()
+    }
+
+    fun onSearchTextChange(searchText: String) {
+        if (searchText == _searchTextStateFlow.value) {
+            return
+        }
+
+        _searchTextStateFlow.value = searchText
+
+        if (searchText.isBlank()) {
+            _quotesListStateFlow.value = fullQuoteList
+            return
+        }
+
+        _quotesListStateFlow.value = fullQuoteList.filter { quote ->
+            val containsInTitle = quote.titleText.contains(
+                other = searchText,
+                ignoreCase = true
+            )
+
+            val containsInSubtitle = quote.subtitleText.contains(
+                other = searchText,
+                ignoreCase = true
+            )
+
+            containsInTitle || containsInSubtitle
+        }
+    }
+
+    private fun updateQuotesList() {
+        fullQuoteList = quotesInteractor.getQuotesList()
+
+        _quotesListStateFlow.value = fullQuoteList
     }
 }
